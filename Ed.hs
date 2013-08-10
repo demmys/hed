633f7183157e5d8120a99ffcd48be063ef14a9f0
Atsuki Demizu <demmys.atsuki@gmail.com>
2013-08-10 23:15:46 +0900

    implemeted: move line number

--- Ed.hs
+++ Ed.hs
@@ -2,15 +2,21 @@ module Ed (ed) where
 
 import System.IO
 import System.Exit (exitSuccess)
+import Data.Char (isDigit)
 
 ed :: String -> Handle -> IO ()
-ed p h = do putStr p
-            hFlush stdout
-            cmd <- getLine
-            parseCommand h cmd
-            ed p h
+ed p h = running 1 p h
+    where running n p h = do putStr p
+                             hFlush stdout
+                             cmd <- getLine
+                             n' <- parseCommand n h cmd
+                             running n' p h
 
 
-parseCommand :: Handle -> String -> IO ()
-parseCommand _ "q" = exitSuccess
-parseCommand _ _ = do putStrLn "?"
+parseCommand :: Integer -> Handle -> String -> IO Integer
+parseCommand _ h "q" = hClose h >> exitSuccess
+parseCommand n _ ".=" = putStrLn (show n) >> return n
+parseCommand n h s | isDigital s = return (read s :: Integer)
+parseCommand n _ _ = putStrLn "?" >> return n
+
+isDigital = and . map isDigit
